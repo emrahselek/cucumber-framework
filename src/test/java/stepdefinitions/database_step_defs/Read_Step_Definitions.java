99786f0610b15a28a4c1384c92fdea0946f59b35
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2021-11-09 20:11:15 -0700

    "es"

--- src/test/java/stepdefinitions/database_step_defs/Read_Step_Definitions.java
+++ src/test/java/stepdefinitions/database_step_defs/Read_Step_Definitions.java
@@ -1,19 +1,13 @@
 package stepdefinitions.database_step_defs;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import utilities.DBUtils;
-import utilities.ReusableMethods;
-
 import java.sql.SQLException;
-
 public class Read_Step_Definitions {
     @Given("user connects to the database")
     public void user_connects_to_the_database() {
         DBUtils.createConnection();
-
     }
-
     @Given("user gets the {string} from {string} table")
     public void user_gets_the_from_table(String column, String table) {
 //        Writing the query
@@ -22,26 +16,25 @@ public class Read_Step_Definitions {
         DBUtils.executeQuery(myDynamicQuery);
 //      I am at Price column on the Hotel Room(column = Price, table = tHOTELROOM)
     }
-
     @Given("user reads all of the {string} column data")
     public void user_reads_all_of_the_column_data(String column) throws SQLException {
-
         DBUtils.getResultset().next();//I was on row 6 now I am on row 7
         Object columnData7 = DBUtils.getResultset().getObject(column);
         System.out.println(columnData7);//620.0000
-
         //        resultset.next(); takes to the next row
         DBUtils.getResultset().next();//I was on row 6 now I am on row 7
         Object columnData8 = DBUtils.getResultset().getObject(column);
         System.out.println(columnData8);//525.00
-
+//        We can get teh data as String as well
+        String columnDataAsString = DBUtils.getResultset().getString(column);
+//        Assert.assertTrue(columnDataAsString.contains("00"));
+        System.out.println(columnDataAsString);
 //        How to get all of the price column data
         while (DBUtils.getResultset().next()){
             Object eachColumnData=DBUtils.getResultset().getObject(column);
             System.out.println(eachColumnData);
         }
     }
-
     @Then("close the connection")
     public void close_the_connection() {
         DBUtils.closeConnection();
